a39bdcb69555fd21bef9c70ac06bd13a3a12cccd
Gary Ashton-Jones <gary@ashton-jones.com.au>
2013-12-09 08:30:38 +1100

    OO version

--- builddat.pl
+++ builddat.pl
@@ -18,81 +18,192 @@
 #     REVISION: ---
 #===============================================================================
 
-use 5.010;
-use Config::Tiny;
-use Text::CSV;
+# genre composer work section part gdid genre2 dcomposer dwork dsection dpart ytid
+# label => $label,
+# lastpublished => '',
+# lastupdated => 1386245606,
+# layout => 'work',
+# title => $filename,
+# type => 'page',
+# url => "/$filename.html",
+package MusicData;
+use Moo::Role;
+use MooX::Types::MooseLike::Base qw(:all);
 use Mojo::Template;
 
-my $mt = Mojo::Template->new;
+has uniqid        => (isa => Str, is => 'rw');
+has label         => (isa => Str, is => 'rw');
+has lastpublished => (isa => Str, is => 'rw', required => 1, default => '');
+has lastupdated   => (isa => Str, is => 'rw', required => 1, default => 1386245606);
+has layout        => (isa => Str, is => 'rw');
+has title         => (isa => Str, is => 'rw');
+has type          => (isa => Str, is => 'rw', default => 'page');
+has url           => (isa => Str, is => 'rw');
 
-my $Config = Config::Tiny->new; #build new.meta details and output at end.
+sub initurl {
+  my $self = shift;
+  $self->url('/'.$self->uniqid.'.html');
+}
+
+sub outputdat {
+  my $self = shift;
+  my $mt = Mojo::Template->new;
+  open my $out, ">", 'page/'.$self->uniqid.'.dat' or die "$self->uniqid: $!";
+  print $out $mt->render_file($self->format.'.mt', $self->sections);
+  close $out;
+}
+
+sub config {
+  return ( $_[0]->uniqid => {
+      label => $_[0]->label,
+      lastpublished => $_[0]->lastpublished,
+      lastupdated => $_[0]->lastupdated,
+      layout => $_[0]->layout,
+      title => $_[0]->title,
+      type => $_[0]->type,
+      url => $_[0]->url,
+    }
+  );
+}
+1;
+
+package Composers;
+use Moo;
+use MooX::Types::MooseLike::Base qw(:all);
+use Config::Tiny;
+has composers => (isa => ArrayRef, is => 'rw');
+
+sub addcomposer {
+  my ($self, $flds) = @_;
+  push @{$self->composers}, Composer->new->initfromflds($flds);
+}
+
+sub outputdat {
+  my $self = shift;
+  $_->outputdat foreach (@$self->composers);
+}
+
+sub outputconfig {
+  my $self = shift;
+  my $Config = Config::Tiny->new;
+  foreach my $composer (@$self->composers) {
+    my ($id, $values) = $composer->config;
+    $Config->{$id} = $values;
+  }
+  $Config->write( 'new.meta', 'utf8' ); #save page data to 'new.meta' file
+}
+1;
+
+package Composer;
+use Moo;
+use MooX::Types::MooseLike::Base qw(:all);
+extends 'MusicData';
+has composer => (isa => Str, is => 'rw', required => 1);
+has dcomposer => (isa => Str, is => 'rw');
+has works => (isa => ArrayRef, is => 'rw');
+has template => (isa => Str, is => 'rw', default => 'composer');
+sub initfromflds {
+  my ($self, $flds) = @_;
+  $self->uniqid($flds->{composer});
+  $self->label($flds->{dcomposer});
+  $self->initurl;
+  push @{$self->works}, MultiWork->new->initfromflds($flds);
+}
+1;
+
+package Work;
+use Moo;
+use MooX::Types::MooseLike::Base qw(:all);
+extends 'MusicData';
+has [qw{genre work dwork} ] => (isa => Str, is => 'rw');
+sub initfromflds {
+  my ($self, $flds) = @_;
+  $self->uniqid($flds->{composer}.'-'.$flds->{work});
+  $self->label($flds->{dcomposer}.' - '.$flds->{dwork});
+  $self->initurl;
+}
+1;
+
+package MultiWork;
+use Moo;
+use MooX::Types::MooseLike::Base qw(:all);
+extends 'MusicData';
+has sections => (isa => ArrayRef[InstanceOf['Section']], is => 'rw');
+has template => (isa => Str, is => 'rw', default => 'mass');
+around 'initfromflds' => sub {
+  my $orig = shift;
+  my ($self, $flds) = @_;
+  $orig->($self, $flds);
+  push @{$self->sections}, Section->new->initfromflds($flds);
+};
+1;
+
+package SingleWork;
+use Moo;
+use MooX::Types::MooseLike::Base qw(:all);
+extends 'Work';
+has section => (isa => InstanceOf['Section'], is => 'rw');
+has template => (isa => Str, is => 'rw', default => 'song');
+around 'initfromflds' => sub {
+  my $orig = shift;
+  my ($self, $flds) = @_;
+  $orig->($self, $flds);
+  $self->section(Section->new->initfromflds($flds));
+};
+1;
+
+package Section;
+use Moo;
+use MooX::Types::MooseLike::Base qw(:all);
+has parts => (isa => ArrayRef, is => 'rw');
+has [qw{section dsection} ] => (isa => Str, is => 'rw');
+sub initfromflds {
+  my ($self, $flds) = @_;
+  push @{$self->parts}, Part->new->initfromflds($flds);
+}
+1;
+
+package Part;
+use Moo::Role;
+use MooX::Types::MooseLike::Base qw(:all);
+has [qw{part gdid dpart ytid} ] => (isa => Str, is => 'rw');
+sub initfromflds {
+  my ($self, $flds) = @_;
+}
+1;
+
+package main;
+use 5.010;
+use Text::CSV;
 
 my $csv = Text::CSV->new ( { binary => 1, sep_char => "\t", } )  # should set binary attribute.
   or die "Cannot use CSV: ".Text::CSV->error_diag ();
 my @names = (qw{genre composer work section part gdid genre2 dcomposer dwork dsection dpart ytid});
 $csv->column_names(@names);
 open my $fh, "<:encoding(utf8)", "gdytmerge.csv" or die "gdytmerge.csv: $!";
-$,="; ";
-my %composers;
-my @sections;
+
+my $composers = Composers->new();
 my $prevhr = {};
 while ( my $hr = $csv->getline_hr( $fh ) ) {
   next unless $hr->{work}; #ignore unparseable entries
 
   if ($hr->{composer} ne $prevhr->{composer}) {
-    if ($prevhr->{composer}) {
-      outputdat('comp', $prevhr, $mt, \@sections);
-      @sections = ();
-    }
-  }
-  if ($hr->{work} ne $prevhr->{work}) {
-    if ($prevhr->{work}) {
-      outputdat('song', $prevhr, $mt, \@sections);
-      @sections = ();
-    }
+    $composers->addcomposer($hr);
+  } elsif ($hr->{work} ne $prevhr->{work}) {
+    $composers->addwork($hr);
   } elsif ($hr->{section} ne $prevhr->{section}) {
-    if ($prevhr->{section}) {
-      if ($prevhr->{genre} ne 'mass') {
-        #output single section template
-        outputdat('song', $prevhr, $mt, \@sections);
-        @sections = ();
-      }
-    }
+    $composers->addsection($hr);
   }
-  #normal processing: same composer, work, section
-  push @sections, $hr;
+  $composers->addpart($hr);
   $prevhr = $hr;
 }
-outputdat('song', $prevhr, $mt, \@sections);
-outputdat('comp', $prevhr, $mt, \@sections);
 
 $csv->eof or $csv->error_diag();
 close $fh;
-$Config->write( 'new.meta', 'utf8' ); #save page data to 'new.meta' file
 
-sub outputdat {
-  my ($type, $ph, $mt, $sections) = @_;
-  my $filename = $ph->{composer}.($type ne 'comp' ? '-'.$ph->{work} : '');
-  open my $out, ">", "page/$filename.dat" or die "$filename: $!";
-  print $out $mt->render_file(
-    ($type eq 'comp' ? 'comp'
-                     : ($ph->{genre} eq 'mass' ? 'mass'
-                                               : 'song')
-  ).'.mt', $sections);
-  close $out;
-  my $label = ($ph->{dcomposer} ?$ph->{dcomposer} :$ph->{composer} ).
-              ($type ne 'comp' ? ' - '.($ph->{dwork} ?$ph->{dwork} :$ph->{work}) : '' );
-  $Config->{$filename} = {
-    label => $label,
-    lastpublished => '',
-    lastupdated => 1386245606,
-    layout => 'work',
-    title => $filename,
-    type => 'page',
-    url => "/$filename.html",
-  };
-  1;
-}
+#output .dat files and new.meta data
+$composers->outputdat;
+$composers->outputconfig;
 
 # vi:ai:et:sw=2 ts=2
 
